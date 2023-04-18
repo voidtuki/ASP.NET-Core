73485a1b9a5f6469744ef00d0117a1756c59d252
Zhou Tianyu <3038279993@qq.com>
2023-04-19 07:12:55 +0800

    Win

---
+++ Project2/MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+            {
+                //查询任何影片
+                if (context.Movie.Any())
+                {
+                    return; //如果数据库有种子了
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+
+                );
+                context.SaveChanges();
+
+
+            }
+        }
+    }
+}
