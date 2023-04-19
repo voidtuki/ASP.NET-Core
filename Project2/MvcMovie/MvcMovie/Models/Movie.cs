19fb1b9c132713f185e1e2eec35d31c37c1ec416
Zhou Tianyu <3038279993@qq.com>
2023-04-20 02:28:32 +0800

    Proejct2

---
+++ Project2/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+
+        //规则验证
+        //组合特性
+        public int Id { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
+        public string? Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1,100),DataType(DataType.Currency),Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"),Required,StringLength(30)]
+        public string? Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"),StringLength(5),Required]
+        public string? Rating { get; set; }
+    }
+
+}
